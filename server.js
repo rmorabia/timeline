a9483e0b1574e3102a9cd57beda884f643ce1549
Radhika Morabia <radhika.morabia@l-tron.com>
2019-04-25 11:32:16 -0400

    Get ready for Heroku

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+const path = require('path')
 const express = require('express')
 const app = express()
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 const events = require('./data')
 const { check, validationResult } = require('express-validator/check')
 
 app.use(express.json())
 
 app.listen(port, () => console.log(`Express is working at port ${port}.`))
+
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('/api', (req, res) => {
   res.send(events)
